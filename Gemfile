28e9df55029c63497dfdafa965fd5b44af5b7fe9
Kelvin Lee <kelvin_lee@leaderradio.cn>
2019-03-29 16:48:35 +0800

    ready for preso

--- Gemfile
+++ Gemfile
@@ -3,6 +3,9 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby '2.5.3'
 
+gem 'faker', :git => 'https://github.com/stympy/faker.git', :branch => 'master'
+gem 'carrierwave', '~> 1.2'
+gem 'cloudinary', '~> 1.9.1'
 gem 'dotenv-rails', groups: [:development, :test]
 gem 'autoprefixer-rails'
 gem 'font-awesome-sass', '~> 5.6.1'
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,20 +1,47 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+require 'open-uri'
+
+Ingredient.delete_all if Rails.env.development?
+Cocktail.delete_all if Rails.env.development?
+
+# url = "https://raw.githubusercontent.com/maltyeva/iba-cocktails/master/recipes.json"
+
+
+# cocktails_array = JSON.parse(open(url).read)
+
+
+# cocktails_array.each do |cocktail|
+#   c = Cocktail.create!(name: cocktail["name"])
+#   p "Added #{c.name} to the list"
+#   cocktail["ingredients"].each do |ing|
+#     unless ing["ingredient"].nil?
+#       i = Ingredient.find_or_create_by!(name: ing["ingredient"])
+#       p "Added #{i.name} to the list"
+#       Dose.create(description: ing["amount"].to_s + " " + ing["unit"], ingredient: i, cocktail: c )
+#     end
+#   end
+# end
+# url = "http://static.giantbomb.com/uploads/original/9/99864/2419866-nes_console_set.png"
+# article = Article.new(title: 'NES', body: "A great console")
+# article.remote_photo_url = url
+# article.save
+
 
 Ingredient.create(name: "lemon")
 Ingredient.create(name: "ice")
 Ingredient.create(name: "mint leaves")
 
-Cocktail.create(name: "Long Island")
-Cocktail.create(name: "Negroni")
-Cocktail.create(name: "Old-fashioned")
-Cocktail.create(name: "Moscow mule")
-Cocktail.create(name: "Manhattan")
-Cocktail.create(name: "Mojito")
+# photo_url = Faker::LoremFlickr.image("200x150", ['beverage'])
+photo_url = "http://loremflickr.com/200/150/beverage"
+Cocktail.create(name: "Long Island", remote_photo_url: photo_url)
+# li.remote_photo_url = photo_url
+# li.save
+
+Cocktail.create(name: "Negroni", remote_photo_url: photo_url)
+Cocktail.create(name: "Old-fashioned", remote_photo_url: photo_url)
+Cocktail.create(name: "Moscow mule", remote_photo_url: photo_url)
+Cocktail.create(name: "Manhattan", remote_photo_url: photo_url)
+Cocktail.create(name: "Mojito", remote_photo_urç: photo_url)
+
 
 # Dose.create(description: "one dash")
